b1a2c6b6e2fada51bdfb2a80248bda6556a5eff5
takahiro-gido <takahiro.gido@thedesignium.com>
2020-10-08 19:18:12 +0900

    ログイン画面をCognitoのMFA（TOTP）認証で実装

--- src/main.js
+++ src/main.js
@@ -1,13 +1,11 @@
 import Vue from "vue";
 import App from "./App.vue";
-import "./registerServiceWorker";
-import router from "./router";
-import store from "./store";
-
-Vue.config.productionTip = false;
-
+import Amplify, * as AmplifyModules from "aws-amplify";
+import { AmplifyPlugin } from "aws-amplify-vue";
+import aws_exports from "./aws-exports";
+Amplify.configure(aws_exports);
+Vue.use(AmplifyPlugin, AmplifyModules);
+// It's important that you instantiate the Vue instance after calling Vue.use!
 new Vue({
-  router,
-  store,
   render: h => h(App)
 }).$mount("#app");
