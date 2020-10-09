8feb9fd8f4789450ff2aea4aa305fb468f4c242a
takahiro-gido <takahiro.gido@thedesignium.com>
2020-10-09 19:43:50 +0900

    ログイン画面を既存のAWS Cognito資産を利用して実装

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,49 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <amplify-authenticator
+      v-if="authState !== 'signedIn'"
+    ></amplify-authenticator>
+    <div v-if="authState === 'signedIn' && user">
+      <amplify-sign-out></amplify-sign-out>
+      <div>Hello, {{ user.username }}</div>
     </div>
-    <router-view />
   </div>
 </template>
-
+<script>
+import { onAuthUIStateChange } from "@aws-amplify/ui-components";
+import { Auth } from "@aws-amplify/auth";
+import { AmplifyEventBus } from "aws-amplify-vue";
+export default {
+  name: "app",
+  async beforeCreate() {
+    try {
+      this.user = await Auth.currentAuthenticatedUser();
+      this.authState = "signedIn";
+    } catch (err) {
+      this.authState = undefined;
+    }
+    // 認証ステータスが変わった時に呼び出されるイベントを登録
+    AmplifyEventBus.$on("authState", async info => {
+      if (info === "signedIn") {
+        this.authState = "signedIn";
+        this.user = await Auth.currentAuthenticatedUser();
+      } else {
+        this.authState = undefined;
+        this.user = undefined;
+      }
+    });
+  },
+  data() {
+    return {
+      user: undefined,
+      authState: undefined
+    };
+  },
+  beforeDestroy() {
+    return onAuthUIStateChange;
+  }
+};
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -15,18 +51,6 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+  margin-top: 60px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,13 +1,17 @@
 import Vue from "vue";
 import App from "./App.vue";
-import "./registerServiceWorker";
-import router from "./router";
-import store from "./store";
-
-Vue.config.productionTip = false;
-
+import Amplify, * as AmplifyModules from "aws-amplify";
+import { AmplifyPlugin } from "aws-amplify-vue";
+Amplify.configure({
+  Auth: {
+    identityPoolId: "ap-northeast-1:f8ede2ee-1d31-465c-a253-82e47c9a9f39",
+    region: "ap-northeast-1",
+    userPoolId: "ap-northeast-1_kybq3xDDy",
+    userPoolWebClientId: "1n81qmdnhgqb4kohcjgh2muv22"
+  }
+});
+Vue.use(AmplifyPlugin, AmplifyModules);
+// It's important that you instantiate the Vue instance after calling Vue.use!
 new Vue({
-  router,
-  store,
   render: h => h(App)
 }).$mount("#app");
