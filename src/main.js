1109891276f12f7cb41c728f7ef369f98c8ef3ad
takahiro-gido <takahiro.gido@thedesignium.com>
2020-10-27 15:35:18 +0900

    Amplifyの設定をdevelopに合わせて変更

--- src/main.js
+++ src/main.js
@@ -4,14 +4,14 @@ import Amplify, * as AmplifyModules from "aws-amplify";
 import { AmplifyPlugin } from "aws-amplify-vue";
 Amplify.configure({
   Auth: {
-    identityPoolId: "ap-northeast-1:3f35d2b1-ea5e-4c5e-80d0-9a895002f766",
+    identityPoolId: "ap-northeast-1:4bd11451-b5c0-438d-b8fe-19ba6d9813a9",
     region: "ap-northeast-1",
-    userPoolId: "ap-northeast-1_NvGqDNaHr",
-    userPoolWebClientId: "1vdlkfb95giejvqe2o6nimoffr"
+    userPoolId: "ap-northeast-1_c80u1Vi6n",
+    userPoolWebClientId: "52kpl8617tu0q9fn0ef552cvs9"
   },
   API: {
     aws_appsync_graphqlEndpoint:
-      "https://pkfisbd2qreu7bgrnun2aoqqj4.appsync-api.ap-northeast-1.amazonaws.com/graphql",
+      "https://yh32zm65mjgyvijfn2x5pnq65y.appsync-api.ap-northeast-1.amazonaws.com/graphql",
     aws_appsync_region: "ap-northeast-1",
     aws_appsync_authenticationType: "AWS_IAM"
   }
