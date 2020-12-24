ace7ff67062f79b890280c7750c203f97dc9d99c
takahiro-gido <takahiro.gido@thedesignium.com>
2020-12-24 14:18:52 +0900

    Cognitoの接続情報更新

--- src/main.js
+++ src/main.js
@@ -4,14 +4,14 @@ import Amplify, * as AmplifyModules from "aws-amplify";
 import { AmplifyPlugin } from "aws-amplify-vue";
 Amplify.configure({
   Auth: {
-    identityPoolId: "ap-northeast-1:4bd11451-b5c0-438d-b8fe-19ba6d9813a9",
+    identityPoolId: "ap-northeast-1:9b5743e3-6443-4017-bf56-c55c2477951e",
     region: "ap-northeast-1",
-    userPoolId: "ap-northeast-1_c80u1Vi6n",
-    userPoolWebClientId: "52kpl8617tu0q9fn0ef552cvs9"
+    userPoolId: "ap-northeast-1_h6sr4o6Dy",
+    userPoolWebClientId: "4a971pg3o6pulnvelk2q285u5d"
   },
   API: {
     aws_appsync_graphqlEndpoint:
-      "https://yh32zm65mjgyvijfn2x5pnq65y.appsync-api.ap-northeast-1.amazonaws.com/graphql",
+      "https://hsmxyvbyfvaypkht5dphima6qe.appsync-api.ap-northeast-1.amazonaws.com/graphql",
     aws_appsync_region: "ap-northeast-1",
     aws_appsync_authenticationType: "AWS_IAM"
   }
